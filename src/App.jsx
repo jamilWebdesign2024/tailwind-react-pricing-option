8f88e64232863b9cd487a1203c910ce6042d0257
jamilWebdesign2024 <jamiluddinjishan@gmail.com>
2025-04-19 02:22:49 +0600

    toggle

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import DaisyNav from './Components/DaisyNav/DaisyNav'
+import Navbar from './Components/Navbar/Navbar'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <header>
+      <Navbar></Navbar>
+    <DaisyNav></DaisyNav>
+    </header>
+    <main>
+
+    </main>
+    </>
+  )
+}
+
+export default App
